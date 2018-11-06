145955cf4e008e7e188ca9219bf30cb41328e598
ZeonNetwork <znccoin@gmail.com>
2018-11-06 18:43:56 +0300

    cp and init

---
+++ scripts/deploy.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+set -ev
+
+function deploy {
+	# $1 server url
+	# $2 dest directory
+	LOCAL_PATH="${TRAVIS_BUILD_DIR}/"
+	SSH_PARAMS="ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
+
+	rsync -avz -e "${SSH_PARAMS}" "${LOCAL_PATH}" cicd@"$1":"$2" --delete \
+		--exclude 'deploy_rsa' \
+		--exclude '.git' \
+		--exclude 'npm-debug.log' \
+		--exclude 'coverage' \
+		--exclude '.nyc_output' \
+		--exclude '.env' \
+		--exclude 'storage' \
+		;
+}
+
+
+if [ "${TRAVIS_BRANCH}" == "master" ]; then
+	deploy  "185.25.117.12" "/var/www/zeon/backend/"
+	deploy  "185.233.116.208" "/var/www/zeon/backend/"
+fi
+
+exit 0;
